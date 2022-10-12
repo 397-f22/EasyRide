a28ec2b13f9d435869e76032b1826c0dee7e892b
James Eidson <eidsonje@icloud.com>
2022-10-11 20:06:11 -0500

    added router + minor style fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { RideList } from "./components/RideList";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <RideList />
-    </div>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<RideList />}></Route>
+      <Route path="/myrides"></Route>
+      <Route path="/add"></Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
