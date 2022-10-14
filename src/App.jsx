e82a2b051d1e3d2c265911add03e701855d17dbe
Vedant Apte <vvapte@ucdavis.edu>
2022-10-13 19:50:08 -0500

    Authentication started

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,46 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AddRide } from "./components/AddRide";
 import { Header } from "./components/Header";
 import { RideList } from "./components/RideList";
+import Landing from "./components/Landing";
 
 const App = () => (
   <BrowserRouter>
-    <Header />
     <Routes>
-      <Route path="/" element={<RideList />}></Route>
-      <Route path="/myrides"></Route>
-      <Route path="/add" element={<AddRide />}></Route>
+      <Route
+        path="/"
+        element={
+          <div>
+            <Header showAddRides={false} />
+            <Landing />
+          </div>
+        }
+      ></Route>
+      <Route
+        path="/allRides"
+        element={
+          <div>
+            <Header showAddRides={true} />
+            <RideList />
+          </div>
+        }
+      ></Route>
+      <Route
+        path="/myRides"
+        element={
+          <div>
+            <Header showAddRides={true} />
+          </div>
+        }
+      ></Route>
+      <Route
+        path="/addRide"
+        element={
+          <div>
+            <Header showAddRides={false} />
+            <AddRide />
+          </div>
+        }
+      ></Route>
     </Routes>
   </BrowserRouter>
 );
